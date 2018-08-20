ccac68a9c6e0322d9d1690e157d128d70104e5b9
Jaakko Sadeharju <jaakko.sadeharju@gmail.com>
2018-08-20 21:10:19 +0300

    Added sortSelections

--- src/extension.ts
+++ src/extension.ts
@@ -31,6 +31,28 @@ export function activate(context: vscode.ExtensionContext) {
     });
 
     context.subscriptions.push(disposable);
+
+    let sort = vscode.commands.registerCommand('extension.sortSelections', () => {
+        // The code you place here will be executed every time your command is executed
+
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
+
+        let selections = editor.selections;
+        editor.edit(editBuilder => {
+            let sorted = [...selections].sort((a,b) => (editor.document.getText(a) < editor.document.getText(b) ? -1 : 1));
+            selections.forEach((s, i) => {
+                let range = new Range(s.start, s.end);
+                if (editor) {
+                    editBuilder.replace(range, editor.document.getText(sorted[i]));
+                }
+            });
+        });
+    });
+
+    context.subscriptions.push(sort);
 }
 
 // this method is called when your extension is deactivated

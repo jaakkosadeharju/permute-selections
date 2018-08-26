4878818e3f2aefacaefa647e5060f6a3a6b3a6e9
Jaakko Sadeharju <jaakko.sadeharju@gmail.com>
2018-08-26 22:25:04 +0300

    Travis CI

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -5,11 +5,6 @@
 
 // The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-// import * as vscode from 'vscode';
-
 import { sortAsc } from "../sort_asc";
 import { sortDesc } from "../sort_desc";
 import { reverse } from "../reverse";
